49766b1a1b8e5349c8dc84254eaaafa6f68c6089
Amin Ghasemi <mmnghasemi@gmail.com>
2025-07-05 13:10:02 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SqlInjectionSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Search in producs : ");
+            string input = Console.ReadLine();
+        }
+    }
+}
